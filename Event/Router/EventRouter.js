ab71f514ba9fdb72e359428755506b37ba0cba42
ahlem <ahlemammar16@gmail.com>
2022-08-14 04:05:59 +0100

    manipulate event's expenses

--- Event/Router/EventRouter.js
+++ Event/Router/EventRouter.js
@@ -14,13 +14,16 @@ const {
     deleteMemberFromEvent,
     addSponsorToEvent,
     deleteSponsorFromEvent,
-    showAllSponsors
+    showAllSponsors,
+    addExpensesToEvent,
+    showAllExpenses
 } = require('../Controller/EventController')
 
 router.route('/').get(getAll).post(createEvent)
 router.route('/:id').get(getOneEvent).patch(updateEvent).delete(deleteEvent)
 router.route('/:id/Member').post(addMemberToEvent).get(showAllMembers).delete(deleteMemberFromEvent)
 router.route('/:id/sponsor').post(addSponsorToEvent).get(showAllSponsors).delete(deleteSponsorFromEvent)
+router.route('/:id/expense').post(addExpensesToEvent).get(showAllExpenses)
 
 
 module.exports=router
